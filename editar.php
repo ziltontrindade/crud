fe4cb03ad07edebae2203f951b68ed71c6e5b60d
Zilton Cordeiro <ztc941@gmail.com>
2022-07-28 17:47:59 -0300

    iniciado Update do crud

---
+++ editar.php
@@ -0,0 +1,44 @@
+<?php 
+require 'config.php';
+
+$info = [];
+
+$id = filter_input(INPUT_GET, 'id');
+if($id){
+    $sql = $pdo->prepare("SELECT * FROM usuarios WHERE id = :id");
+    $sql->bindValue(':id',$id);
+    $sql->execute();
+
+    if ($sql->rowCount()>0) {
+        
+        $info = $sql->fetch(PDO::FETCH_ASSOC);
+
+
+
+    }else{
+        header("Location: index.php");
+    }
+
+
+
+}else{
+    header("Location: index.php");
+    exit;
+}
+?>
+
+
+<h1>EDITAR USUARIO</h1>
+
+<form action="editar_action.php" method="POST">
+    <label>
+        Nome: <br>
+        <input type="text" name="name" value="<?=$info['nome'];?>" />
+    </label><br><br>
+    <label>
+        Email: <br>
+        <input type="email" name="email" value="<?=$info['email'];?>"/>
+    </label><br><br>
+    <label>
+    <input type="submit" value="SALVAR">
+</form>
