e0dcb57522fd1850a33c83add8c71913ccca6a2b
Zilton Cordeiro <ztc941@gmail.com>
2022-08-02 20:50:19 -0300

    index.php atualizado

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 require 'config.php';
+require 'dao/UsuarioDaoMysql.php';
 
-$lista = [];
-$sql = $pdo->query("SELECT * FROM usuarios");
-if($sql->rowCount() > 0) {
-    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
-}
+
+$usuarioDao = new UsuarioDAOMySQL($pdo);
+$lista = $usuarioDao->findAll();
+
 ?>
-
 <a href="adicionar.php">ADICIONAR USUÁRIO</a>
 
 <table border="1" width="100%">
@@ -19,12 +18,12 @@ if($sql->rowCount() > 0) {
     </tr>
     <?php foreach($lista as $usuario): ?>
         <tr>
-            <td><?=$usuario['id'];?></td>
-            <td><?=$usuario['nome'];?></td>
-            <td><?=$usuario['email'];?></td>
+            <td><?=$usuario->getId();?></td>
+            <td><?=$usuario->getNome();?></td>
+            <td><?=$usuario->getEmail();?></td>
             <td>
-                <a href="editar.php?id=<?=$usuario['id'];?>">[ Editar ]</a>
-                <a href="excluir.php?id=<?=$usuario['id'];?>" onclick="return confirm('Tem certeza que deseja excluir?')">[ Excluir ]</a>
+                <a href="editar.php?id=<?=$usuario->getId();?>">[ Editar ]</a>
+                <a href="excluir.php?id=<?=$usuario->getId();?>" onclick="return confirm('Tem certeza que deseja excluir?')">[ Excluir ]</a>
             </td>
         </tr>
     <?php endforeach; ?>
